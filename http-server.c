e4d7fe7e3539c7d49ef58a69e28bc04c0ffae1fa
lewcol <148833753+lewcol@users.noreply.github.com>
2025-07-29 23:56:01 +0100

    Change port to string expected by getaddrinfo

--- http-server.c
+++ http-server.c
@@ -6,32 +6,35 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
-#define DEFAULT_PORT 8080
+#define DEFAULT_PORT "8080"
 
 int main(int argc, char **argv) {
-    int port, status;
+    char *port;
+    int status;
     struct addrinfo hints;
     struct addrinfo *serverinfo;
 
     // Determine port number
     if (argc > 1) {
-        if ((port = atoi(argv[1])) > 0) {
-            printf("Using Port %d\n", port);
+        if (atoi(argv[1]) > 0) {
+            printf("Using Port %s\n", argv[1]);
+            port = argv[1];
         } else {
-            printf("Invalid port %d, using default port %d\n", port, DEFAULT_PORT);
+            printf("Invalid port %s, using default port %s\n", argv[1], DEFAULT_PORT);
             port = DEFAULT_PORT;
         }
     } else {
-        printf("No port provided, using default port %d\n", DEFAULT_PORT);
+        printf("No port provided, using default port %s\n", DEFAULT_PORT);
         port = DEFAULT_PORT;
     }
 
+    printf("%s", port);
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    if ((status = getaddrinfo(NULL, "80", &hints, &serverinfo)) != 0) {
+    if ((status = getaddrinfo(NULL, port, &hints, &serverinfo)) != 0) {
         fprintf(stderr, "Error: %s\n", gai_strerror(status));
         exit(status);
     }
